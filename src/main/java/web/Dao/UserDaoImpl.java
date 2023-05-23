f43748d8e7a66b49de80c4e37f3908e80fa2fb6b
Mikhail <120327908+MegaMasteR32@users.noreply.github.com>
2023-05-23 17:22:27 +0300

    EntityMananger2

--- src/main/java/web/Dao/UserDaoImpl.java
+++ src/main/java/web/Dao/UserDaoImpl.java
@@ -21,28 +21,36 @@ class UserDaoImpl implements UserDao {
     @Override
     @Transactional
     public void saveUser(User user) {
-        entityManager.getCurrentSession().saveOrUpdate(user);
+//        entityManager.getCurrentSession().saveOrUpdate(user);
+        entityManager.persist(user);
+        entityManager.flush();
     }
 
     @Override
     @Transactional
     public void delete(int id) {
-        TypedQuery<User> query = entityManager.getCurrentSession().createQuery("delete from User where id =:userID");
-        query.setParameter("userID", id);
-        query.executeUpdate();
+//        TypedQuery<User> query = entityManager.getCurrentSession().createQuery("delete from User where id =:userID");
+//        query.setParameter("userID", id);
+//        query.executeUpdate();
+        entityManager.createQuery("delete from User where id=: id")
+                .setParameter("id", id)
+                .executeUpdate();
+        entityManager.flush();
 
     }
 
     @Override
     @Transactional
     public User getById(int id) {
-        return entityManager.getCurrentSession().get(User.class, id);
+//        return entityManager.getCurrentSession().get(User.class, id);
+        return entityManager.find(User.class, id);
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<User> getAllUsers() {
-        TypedQuery<User> query = entityManager.getCurrentSession().createQuery("from User");
-        return query.getResultList();
+//        TypedQuery<User> query = entityManager.getCurrentSession().createQuery("from User");
+//        return query.getResultList();
+        return entityManager.createQuery("from User").getResultList();
     }
 }
